302b66d08848a78985bc8de80796c5b50d6828b8
PoomPK <poompk2542@gmail.com>
2022-02-12 20:27:45 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:h7/test_page.dart';
+
+import 'next_page.dart';
 
 
 void main() {
-  runApp(MyApp());
+  const app = MyApp();
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // callback method
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: DynamicChange(),
+    );
+  }
+}
+
+class DynamicChange extends StatefulWidget  {
+  @override
+  HomePage createState() => HomePage();
+}
+class HomePage extends State<DynamicChange> {
+
+  String _pin = '';
+  @override
+  Widget build(BuildContext context) {
+
     return MaterialApp(
         home: Scaffold(
             body: Container(
@@ -24,14 +51,36 @@ class MyApp extends StatelessWidget {
                             size: 80.0,           // ขนาดไอคอน
                             color: Colors.blueGrey,   // สีไอคอน
                           ),
-                          Text('กรุณาใส่รหัสผ่าน', style:
+                          Text('Login', style:
                             TextStyle(
                                 fontSize: 26.0,
                                 color: Colors.blueGrey,
                                 fontWeight: FontWeight.w800)
                             ),
+                          Text('Enter PIN to login', style:
+                          TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.blueGrey,
+                              fontWeight: FontWeight.w800)
+                          ),
+
                         ],
                       ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 80),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        _StatusInput(1),
+                        _StatusInput(2),
+                        _StatusInput(3),
+                        _StatusInput(4),
+                        _StatusInput(5),
+                        _StatusInput(6),
+                      ],
                     ),
                   ),
                   Expanded(
@@ -41,28 +90,29 @@ class MyApp extends StatelessWidget {
                       child: Column(
 
                         children: [
+
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                              children: [
-                               _buildCircle(1),
-                               _buildCircle(2),
-                               _buildCircle(3),
+                               _buildCircle(context,1),
+                               _buildCircle(context,2),
+                               _buildCircle(context,3),
                              ],
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              _buildCircle(4),
-                              _buildCircle(5),
-                              _buildCircle(6),
+                              _buildCircle(context,4),
+                              _buildCircle(context,5),
+                              _buildCircle(context,6),
                             ],
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              _buildCircle(7),
-                              _buildCircle(8),
-                              _buildCircle(9),
+                              _buildCircle(context,7),
+                              _buildCircle(context,8),
+                              _buildCircle(context,9),
                             ],
                           ),
                           Row(
@@ -71,79 +121,147 @@ class MyApp extends StatelessWidget {
 
                               Padding(
                                 padding: const EdgeInsets.only(left: 85),
-                                child: _buildCircle(0),
+                                child: _buildCircle(context,0),
                               ),
-                              _buildBackSpace(),
+                              _buildBackSpace(context),
 
 
                             ],
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-
-
-
-
-                            ],
+                        TextButton(onPressed: (){
+                          print('pressed forget password');
+                        },
+                          child:Text('ลืมรหัสผ่าน', style:
+                          TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.w800,)
                           ),
-                          Expanded(
-                            child: TextButton(onPressed: (){
-                              print('pressed forget password');
-                            },
-                              child:Text('ลืมรหัสผ่าน', style:
-                              TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.blue,
-                                fontWeight: FontWeight.w800,)
-                              ),),
-                          )
+                        ),
                         ],
                       ),
+
                     ),
                   )
+
+
                 ],
               ),
             )
         ));
   }
 
-  // วงกลม มี border มีเงา
-  Widget _buildCircle(int num) {
+  bool _CheckPIN() {
+    return _pin == '123456';
+  }
+_validate() {
+    if(_pin.length == 6) {
+      if(_CheckPIN()) {
+        _pin = '';
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const NextPage()
+          ),
+        );
+
+      } else {
+       print('incorrect');
+        _pin = '';
+
+       showDialog(
+         context: context,
+         barrierDismissible: false,
+         builder: (BuildContext context) {
+           return AlertDialog(
+             title: Text('Incorrect PIN'),
+             content: Text('Please try again.'),
+             actions: [
+               TextButton(
+                 onPressed: () {
+                   Navigator.of(context).pop();
+                 },
+                 child: Text('OK'),
+               ),
+             ],
+           );
+         },
+       );
+      }
+
+    }
+}
+
+  _buildCircle(BuildContext context,int? num) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        child: Text('$num', style:
-        TextStyle(
-            fontSize: 26.0,
-            color: Colors.blueGrey,
-            fontWeight: FontWeight.w800)
-        ),
         width: 75.0,
         height: 75.0,
-        alignment: Alignment.center,
-        //color: Colors.white, // ห้ามกำหนด color ตรงนี้ ถ้าหากกำหนดใน BoxDecoration แล้ว
-        decoration: BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle,
-            border: Border.all(color: Colors.blueGrey, width: 4.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2), // สีเงา
-                offset: Offset(2, 4), // ทิศทางของเงาในแนวนอนและแนวตั้ง ตามลำดับ
-                blurRadius: 4.0,
-                spreadRadius: 2.0,
-              )
-            ]
+        child: RaisedButton(
+          color: Colors.blueGrey,
+          shape: RoundedRectangleBorder(
+
+            borderRadius: BorderRadius.circular(75 / 2),
+          ),
+          focusColor: Colors.blueGrey.shade700,
+          hoverColor: Colors.blueGrey.shade700,
+          highlightColor: Colors.blueGrey,
+          onPressed: () {
+              setState(() {
+                if(_pin.length < 6) {
+                  _pin += '$num';
+
+
+
+                }
+               _validate();
+              });
+
+
+          },
+          child: Center(
+            child: Text(
+              '$num',
+              style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,fontSize: 25),
+            ),
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildBackSpace() {
+  _buildBackSpace(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
+        width: 75.0,
+        height: 75.0,
+
+        child: IconButton(onPressed: (){
+          setState(() {
+            if(_pin.length > 0) {
+              _pin = _pin.substring(0,_pin.length-1);
+            }
+
+          });
+
+        }
+            , icon: Icon(
+            Icons.backspace,         // รูปไอคอน
+            size: 35.0,           // ขนาดไอคอน
+            color: Colors.blueGrey,   // สีไอคอน
+          ),)
+
+      ),
+    );
+  }
+
+  Widget _buildBackSpaceOld() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+
         child: Icon(
           Icons.backspace,         // รูปไอคอน
           size: 35.0,           // ขนาดไอคอน
@@ -157,4 +275,22 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  Widget _StatusInput(int position) {
+    return Container(
+
+      child: Icon(
+        Icons.circle,         // รูปไอคอน
+        size: 35.0,           // ขนาดไอคอน
+        color: _pin.length<position?Colors.blueGrey.shade100:Colors.blueGrey.shade400,   // สีไอคอน
+      ),
+      width: 50.0,
+      height: 50.0,
+      alignment: Alignment.center,
+      //color: Colors.white, // ห้ามกำหนด color ตรงนี้ ถ้าหากกำหนดใน BoxDecoration แล้ว
+
+    );
+  }
+
+
 }
